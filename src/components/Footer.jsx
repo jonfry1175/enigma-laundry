07213dfd942056546ad0bf0e7afecce34d788701
jonfry1175 <odiboen@gmail.com>
2024-05-18 04:22:55 +0700

    update footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-dark text-light text-center p-5 position-relative ">
     <div className="container">
-      <p className="lead">Copyright &copy; 2022 MariTravel.com</p>
+      <p className="lead">Copyright &copy; 2024 EnigmaLaundry.com</p>
 
       <a href="#" className="position-absolute bottom-0 end-0 p-5">
         <i className="bi bi-arrow-up-circle h1"></i>
