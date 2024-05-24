28524eb0f7f6c8b5f398ec9facd7755e4e41ceb2
jonfry1175 <odiboen@gmail.com>
2024-05-25 02:59:08 +0700

    Implement lazy loading for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import { Toaster } from "sonner";
 import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/auth/LoginPage";
-import RegisterPage from "./pages/auth/RegisterPage";
-import NotFound from "./pages/NotFound";
-import Customer from "./pages/dashboard/customers/Customer";
-import Product from "./pages/dashboard/products/Product";
-import Transactions from "./pages/dashboard/transactions/Transactions";
 import Loader from "./components/Loader";
 import { lazy, Suspense } from "react";
-
 const HomePage = lazy(() => import("./pages/HomePage"));
+const LoginPage = lazy(() => import("./pages/auth/LoginPage"));
+const RegisterPage = lazy(() => import("./pages/auth/RegisterPage"));
+const Customer = lazy(() => import("./pages/dashboard/customers/Customer"));
+const Product = lazy(() => import("./pages/dashboard/products/Product"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Transactions = lazy(() =>
+  import("./pages/dashboard/transactions/Transactions")
+);
 
 function App() {
   return (
     <>
       <Toaster position="top-center" />
-      <Routes>
-        <Route path="/" element={
-          <Suspense fallback={<Loader />}>
-            <HomePage />
-          </Suspense>
-        } />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard-customers" element={<Customer />} />
-        <Route path="/dashboard-products" element={<Product />} />
-        <Route path="/dashboard-transaction" element={<Transactions/>} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard-customers" element={<Customer />} />
+          <Route path="/dashboard-products" element={<Product />} />
+          <Route path="/dashboard-transaction" element={<Transactions />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
