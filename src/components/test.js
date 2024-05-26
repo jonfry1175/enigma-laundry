cb5d1027c2fb4d49021e5a391cfb360964325d2e
jonfry1175 <odiboen@gmail.com>
2024-05-27 03:55:45 +0700

    list transaction & detail transaction

--- src/components/test.js
+++ src/components/test.js
@@ -1,13 +1,4 @@
-for (let i = 1; i <= 30; i++) {
-    if (i % 3 === 0 && i % 5 === 0 && i % 10 === 0) {
-        console.log("habis dibagi 3,5,10")
-    } else if (i % 3 === 0 && i % 5 === 0) {
-        console.log("habis dibagi 3,5")
-    } else if (i % 3 === 0) {
-        console.log("habis dibagi 3")
-    } else if (i % 5 === 0) {
-        console.log("habis dibagi 5")
-    } else {
-        console.log(i)
-    }
-}
+var foo = []
+foo.push(1)
+foo.push(2)
+console.log(foo[2])
--- src/pages/dashboard/transactions/Transactions.jsx
+++ src/pages/dashboard/transactions/Transactions.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Sidebar from "../../../components/Sidebar";
-import { Button, Table } from "react-bootstrap";
+import { Button, Table, Modal } from "react-bootstrap";
 import { axiosInstance } from "../../../lib/axios";
 import withAuth from "../../../hoc/withAuth";
 import { useSelector } from "react-redux";
 
 const Transactions = () => {
-  const [transactionData, setTransactionData] = useState([]);
+  const [customerData, setCustomerData] = useState([]);
+  const [selectedCustomer, setSelectedCustomer] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   const token = useSelector((state) => state.auth.token);
 
@@ -16,16 +18,45 @@ const Transactions = () => {
         Authorization: `Bearer ${token}`,
       };
       const response = await axiosInstance.get("/bills", { headers });
-      setTransactionData(response.data.data);
+      const transactions = response.data.data;
+
+      const newCustomerData = {};
+
+      transactions.forEach((transaction) => {
+        const customerId = transaction.customer.id;
+
+        // jika customer belum ada di objek newCustomerData, tambahkan entri baru
+        if (!newCustomerData[customerId]) {
+          newCustomerData[customerId] = {
+            ...transaction.customer, // copy semua properti customer
+            transactions: [], // tambahkan properti transactions
+            transactionCount: 0, // tambahkan properti transactionCount
+          };
+        }
+        // Tambahkan transaksi ke daftar transaksi pelanggan
+        newCustomerData[customerId].transactions.push(transaction);
+        // Tingkatkan jumlah transaksi pelanggan
+        newCustomerData[customerId].transactionCount += 1;
+      });
+
+      setCustomerData(newCustomerData);
       console.log(token);
     } catch (error) {
       console.log(error.message);
     }
   };
 
-  const checkTransactionData = () => {
-    console.log(transactionData);
+  const check = () => {
+    console.table(customerData);
   };
+
+  // ambil detail by customer id
+  const handleClick = (customerData) => {
+    setShowModal(true);
+    setSelectedCustomer(customerData);
+    console.log(customerData);
+  };
+
   useEffect(() => {
     getTransactions();
   }, []);
@@ -38,7 +69,7 @@ const Transactions = () => {
         </div>
         <div className="col">
           <h1 className="text-center"> List Transaksi</h1>
-          <Button onClick={checkTransactionData} variant="primary">
+          <Button onClick={check} variant="primary">
             Cek Transaksi
           </Button>
           <Table striped bordered hover>
@@ -51,17 +82,22 @@ const Transactions = () => {
               </tr>
             </thead>
             <tbody>
-              {transactionData.map((transaction, index) => (
+              {Object.values(customerData).map((customer, index) => (
                 <tr key={index}>
                   <td>{index + 1}</td>
-                  <td>{transaction.customer.id}</td>
+                  <td>{customer.id}</td>
                   <td>
-                    <span className="fw-bold">{transaction.customer.name}</span>
+                    <span className="fw-bold">{customer.name}</span>
                     <br />
-                    {} Transaksi
+                    {customer.transactionCount} Transaksi
                   </td>
                   <td>
-                    <Button variant="primary">Detail</Button>
+                    <Button
+                      onClick={() => handleClick(customer)}
+                      variant="primary"
+                    >
+                      Detail
+                    </Button>
                   </td>
                 </tr>
               ))}
@@ -69,6 +105,59 @@ const Transactions = () => {
           </Table>
         </div>
       </div>
+
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Riwayat Transaksi a.n {selectedCustomer?.name}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedCustomer && (
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Kode Transaksi</th>
+                  <th>Tanggal Transaksi</th>
+                  <th>QTY</th>
+                  <th>Jenis Laundry</th>
+                  <th>Total Harga</th>
+                </tr>
+              </thead>
+              <tbody>
+                {selectedCustomer.transactions.map((transaction) => (
+                  <tr key={transaction.id}>
+                    <td>{transaction.id}</td>
+                    <td>{transaction.billDate}</td>
+                    <td>
+                      {transaction.billDetails.reduce(
+                        (acc, item) => acc + item.qty,
+                        0
+                      )} 
+                      {/* ambil product.type */}
+                      {transaction.billDetails.map((item) => item.product.type)}
+                    </td>
+                    <td>
+                      {transaction.billDetails.map((item) => item.product.name)}
+                    </td>
+                    <td>
+                      {transaction.billDetails.reduce(
+                        (acc, item) => acc + item.price * item.qty,
+                        0
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
