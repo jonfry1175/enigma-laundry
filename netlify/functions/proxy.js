30d1cca5c01f41ae2d22ac4f0ff19beaa3fc4c7d
jonfry1175 <odiboen@gmail.com>
2024-08-08 03:01:12 +0700

    test

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,10 +1,11 @@
+require = require('esm')(module);
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 exports.handler = createProxyMiddleware({
   target: process.env.LAUNDRY_API_URL,
   changeOrigin: true,
   pathRewrite: {
-    '^/.netlify/functions/proxy': '', // Remove base path
+    '^/api': '', // Remove base path
   },
   onProxyReq: (proxyReq, req, res) => {
     console.log('Proxy Request:', req.method, req.url);
