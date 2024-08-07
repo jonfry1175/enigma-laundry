91e79306ecf8d834c33fa6926e7d68c3da404f49
jonfry1175 <odiboen@gmail.com>
2024-08-08 03:09:02 +0700

    test

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,16 +1,34 @@
-require = require('esm')(module);
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-exports.handler = createProxyMiddleware({
-  target: process.env.LAUNDRY_API_URL,
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': '', // Remove base path
-  },
-  onProxyReq: (proxyReq, req, res) => {
-    console.log('Proxy Request:', req.method, req.url);
-  },
-  onProxyRes: (proxyRes, req, res) => {
-    console.log('Proxy Response:', proxyRes.statusCode, req.url);
-  },
-});
+exports.handler = async (event, context) => {
+  const isPlainObj = (await import('is-plain-obj')).default;
+
+  // Buat middleware proxy
+  const proxyMiddleware = createProxyMiddleware({
+    target: process.env.LAUNDRY_API_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      '^/.netlify/functions/proxy': '', // Hapus path dasar
+    },
+    onProxyReq: (proxyReq, req, res) => {
+      console.log('Proxy Request:', req.method, req.url);
+    },
+    onProxyRes: (proxyRes, req, res) => {
+      console.log('Proxy Response:', proxyRes.statusCode, req.url);
+    },
+  });
+
+  return new Promise((resolve, reject) => {
+    const response = {
+      statusCode: 200,
+      body: '',
+    };
+    proxyMiddleware(event, context, (result) => {
+      if (result instanceof Error) {
+        reject(result);
+      } else {
+        resolve(response);
+      }
+    });
+  });
+};
